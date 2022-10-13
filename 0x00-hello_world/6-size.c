e642fa9702211e8698249130dc91752bfdf66bea
Hilya Ntinda <ntindahilya@gmail.com>
2022-10-13 11:04:08 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  * main - Entry point
  *
@@ -22,5 +20,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-
 }
