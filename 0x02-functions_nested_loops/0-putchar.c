0515d645ebac9e96658247b331f5d02b6a3a1c4e
Hilya Ntinda <ntindahilya@gmail.com>
2022-10-18 04:33:09 -0700

    0-putchar.c updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	char *sh = "_putchar";
+	char c[] = "_putchar";
+	int i;
 
-	while (*sh)
+	for (i = 0; i < 9; i++)
 	{
-		_putchar(*sh);
-		sh++;
+		_putchar(c[i]);
 	}
 	_putchar('\n');
-
 	return (0);
 }
