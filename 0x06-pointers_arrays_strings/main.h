1ada6748b2effb77d9baa314d2a9509a5d4b4be8
Hilya Ntinda <ntindahilya@gmail.com>
2022-10-27 01:16:36 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,9 +12,9 @@ char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
 void print_number(int n);
+int main(void)
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int main(void);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /* MAIN_H */
