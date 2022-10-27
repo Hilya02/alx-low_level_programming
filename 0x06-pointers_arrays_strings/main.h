d1025181136a518c5943272864e0146b239cc797
Hilya Ntinda <ntindahilya@gmail.com>
2022-10-27 00:51:29 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,10 @@ char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int main(void);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
 #endif /* MAIN_H */
